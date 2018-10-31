90a2e4024088f70cc8a895d8c5d81f157fb82d55
Gu Xun <guxun@Gus-MacBook-Pro.local>
2018-10-31 22:31:45 +0900

    add subtraction

--- demo/babymath/babymath.js
+++ demo/babymath/babymath.js
@@ -7,7 +7,29 @@ var drawingApp = (function(){
   
 
   updateClick = function(){
-    textDiv.innerHTML = mathText();
+    // check selection
+    var radios = document.getElementsByName('mathSelection');
+    var selection;
+
+    for (var i = 0, length = radios.length; i < length; i++)
+    {
+      if (radios[i].checked)
+      {
+        selection = radios[i].value;
+        break;
+      }
+    }
+    
+    if(selection == "0")
+    {
+      textDiv.innerHTML = mathAddText();
+    }
+    else
+    {
+      textDiv.innerHTML = mathSubText();
+    }
+    
+    
   },
   
   randInt = function(min, max) {
@@ -22,7 +44,7 @@ var drawingApp = (function(){
     return r;
   },
   
-  mathText = function() {
+  mathAddText = function() {
     var i;
     var body = "";
     for (i = 0; i < 1000; i++) {
@@ -36,11 +58,26 @@ var drawingApp = (function(){
     
     return body;
   },
+  
+  mathSubText = function() {
+    var i;
+    var body = "";
+    for (i = 0; i < 1000; i++) {
+      var num1 = formatNumLen(randInt(5, maxTotal - 1), maxWidth);
+      var num2 = formatNumLen(randInt(1, num1), maxWidth);
+      body += num1 + " - " + num2 + " =        ";
+      if (i % 2 == 1) {
+        body += "\n";
+      }
+    }
+    
+    return body;
+  },
 
   mainExec = function() {
   
     textDiv = document.getElementById('abc');    
-    textDiv.innerHTML = mathText();
+    textDiv.innerHTML = mathAddText();
     
     document.getElementById('btn0').onclick = updateClick;
   };
